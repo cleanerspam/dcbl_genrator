a65512d2f593828c37711029686c592ca24a2e98
cleanerspam <cleanerspam@aol.com>
2020-04-22 13:03:18 +0530

    Updated Genrator Script

--- genrator.sh
+++ genrator.sh
@@ -1,2 +1,20 @@
-echo "#dns-crypt Blacklist Generating Script File"
-Hh
+#!/bin/bash
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+echo -e " \n \n        ${GREEN}   #dns-crypt Blacklist Generating Script File${NC}"
+ 
+echo  "   \n \n           Checking For Updates And Updating"
+pkg upgrade
+
+echo "        \n    \n .           .     Installing/Updating git"
+pkg install git
+
+echo "\n \nInstalling/Updating python"
+pkg install python
+echo "   \n \n                  Checking If dns-crypt Is Installed \n \n "
+if [ -d "$/sdcard/Download" ]; then
+    #Will enter here if $DIRECTORY exists, even if it contains spaces
+    echo "Directory Exists"
+fi
+echo "Does Not Exist"
